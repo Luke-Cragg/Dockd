81dd89adbace34659a0069d76e083ae754312d6c
Luke-Cragg <124095690+Luke-Cragg@users.noreply.github.com>
2023-04-16 21:31:13 +0100

    Firebase database/core

--- dockd/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dockd/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_database
 import location
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
