b3f7719ea5c93586ded022e2894d8665b6a12eea
Luke-Cragg <124095690+Luke-Cragg@users.noreply.github.com>
2023-04-16 21:32:01 +0100

    Adding firebase to initialise the app

--- dockd/lib/main.dart
+++ dockd/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:dockd/Navigation.dart';
 import 'package:dockd/VehicleTracking.dart';
 import 'package:dockd/Vehicles.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:dockd/Home.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
